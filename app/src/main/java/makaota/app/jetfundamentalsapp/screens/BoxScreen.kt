808b1cd729369de4a5b933c04160b52c7357b2d1
makaota <74915165+makaota@users.noreply.github.com>
2022-07-01 16:47:55 +0200

    final chapter 3

---
+++ app/src/main/java/makaota/app/jetfundamentalsapp/screens/BoxScreen.kt
@@ -0,0 +1,50 @@
+package makaota.app.jetfundamentalsapp.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import makaota.app.jetfundamentalsapp.R
+import makaota.app.jetfundamentalsapp.router.BackButtonHandler
+import makaota.app.jetfundamentalsapp.router.JetFundamentalsRouter
+import makaota.app.jetfundamentalsapp.router.Screen
+
+@Composable
+fun BoxScreen() {
+    MyBox()
+
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+@Preview
+fun MyBox(
+    modifier: Modifier = Modifier,
+    contentModifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.first),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.TopStart)
+        )
+        Text(
+            text = stringResource(id = R.string.second),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.Center)
+        )
+        Text(
+            text = stringResource(id = R.string.third),
+            fontSize = 22.sp,
+            modifier = contentModifier.align(Alignment.BottomEnd)
+        )
+    }
+
+}
---
+++ app/src/main/java/makaota/app/jetfundamentalsapp/screens/SurfaceScreen.kt
@@ -0,0 +1,43 @@
+package makaota.app.jetfundamentalsapp.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import makaota.app.jetfundamentalsapp.R
+import makaota.app.jetfundamentalsapp.router.BackButtonHandler
+import makaota.app.jetfundamentalsapp.router.JetFundamentalsRouter
+import makaota.app.jetfundamentalsapp.router.Screen
+
+@Composable
+fun SurfaceScreen(modifier: Modifier = Modifier) {
+
+    Box(modifier = modifier.fillMaxSize()) {
+        MySurface(modifier = modifier.align(Alignment.Center))
+    }
+
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun MySurface(modifier: Modifier) {
+    Surface(
+        modifier = modifier.size(100.dp), // 1
+        color = Color.LightGray, // 2
+        contentColor = colorResource(id =
+        R.color.colorPrimary), // 2
+        elevation = 1.dp, // 3
+        border = BorderStroke(1.dp, Color.Black) // 4
+    ) {
+        MyColumn() // 5
+    }
+}
